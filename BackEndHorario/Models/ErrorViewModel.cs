ff6b9d8f13a3adefc8821212bd52ca9569bd0569
Rodrigo Henriques <rodrigomch7@gmail.com>
2025-05-29 19:01:13 +0100

    versão vazia
    Criacao do projeto

---
+++ BackEndHorario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackEndHorario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
